fbe291c11b685bf0e7070e9c425d407dec37f096
Tom Wood <dexterbyte01@gmail.com>
2024-02-20 17:57:34 +0000

    stylize

--- src/components/SearchForm/index.jsx
+++ src/components/SearchForm/index.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 
 export default function SearchForm({ onFormSubmit, nameInput, setNameInput }){
 
-    const [noSearch, setNoSearch] = useState(false)
+    const [searching, setSearching] = useState("")
 
     const handleNameInput = (e) => {
         setNameInput(e.target.value)
@@ -10,20 +10,26 @@ export default function SearchForm({ onFormSubmit, nameInput, setNameInput }){
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        setNoSearch(true)
-        const pokemonData = await fetch(`https://pokeapi.co/api/v2/pokemon/${nameInput}`)
-        const pokemon = await pokemonData.json()
-        onFormSubmit({
-            name: pokemon.name,
-            type: pokemon.types[0].type.name,
+        try {
+            setSearching("Searching...")
+            const pokemonData = await fetch(`https://pokeapi.co/api/v2/pokemon/${nameInput.toLowerCase()}`)
+            const pokemon = await pokemonData.json()
+            const pokemonTypes = (pokemon.types.length == 2 ? `${pokemon.types[0].type.name.charAt(0).toUpperCase() + pokemon.types[0].type.name.slice(1)}, ${pokemon.types[1].type.name.charAt(0).toUpperCase() + pokemon.types[1].type.name.slice(1)}` : pokemon.types[0].type.name.charAt(0).toUpperCase() + pokemon.types[0].type.name.slice(1))
+            onFormSubmit({
+            name: pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1),
+            type: pokemonTypes,
             image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`
         });
+        setSearching("")
         setNameInput("")
+        } catch (error) {
+            setSearching("Pokemon not found.")
+        }
     }
 
     return(
         <>
-        {!noSearch && <p id="NoSearchYet">No Pokemon searched yet.</p>}
+        <p className="searching">{searching}</p>
         <form onSubmit={handleSubmit}>
             <input value={nameInput} type="text" onChange={handleNameInput}/>
             <button type="submit" onSubmit={handleSubmit}>Search</button>
